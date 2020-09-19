216ca6804430b4289fbd98e1b0bbe37ab5869378
Edwin Savelson <edwinsavelson@Edwins-MacBook-Pro.local>
2020-09-18 19:38:59 -0700

    install airtable and ejs.  create index.ejs in views folder.  index.ejs now displays the state name from airtable.

--- app.js
+++ app.js
@@ -1,6 +1,49 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'ejs');
+
+
+
+// AIRTABLE STUFF
+
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: 'keyCJifn2RC3KCb2g'}).base('appOrPuThUPb5ioAq');
+
+let info;
+
+base('Table 1').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        // console.log('Retrieved', record.get('State'));
+        info = record.get('State');
+        console.log(info);
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
+
+
+
+
+
+
+
+
+
+
+
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -8,7 +51,8 @@ app.use(express.static("public"));
 
 app.get("/" , function(req, res){
   console.log(req);
-  res.sendFile(__dirname + "/state.html");
+  // res.sendFile(__dirname + "/state.html");
+  res.render('index', {newYorkState: info});
 })
 
 app.listen(3000, function(){
