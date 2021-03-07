3fb9346498e727f3b3efafac4dd6b0be316109c0
Edwin Savelson <esavelson279@gmail.com>
2021-03-06 16:12:01 -0800

    Move Member Data to new file. Add Liliana Suero to board.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const nodemailer = require("nodemailer");
 const request = require("request");
 // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
+// REQUIRE DATA FOR BOARD MEMBER PAGE
+const boardMemberData = require("./public/boardMemberData.js");
 
 // RECAPTCHA SITE KEY 6Ld-INoZAAAAACsWVusp03WAfJUuE3u2JHtxWSfs
 
@@ -42,46 +44,6 @@ app.get("/board", function(req, res) {
   res.render('boardmembers')
 })
 
-const boardMemberData = {
-  "Beth-Skelton": {
-    "name": "Beth Skelton - Chair",
-    "img": "BethSkeltonHeadshot.png",
-    "bio": " Beth has over 30 years of experience as a language educator and holds a Master’s Degree in Multicultural Teacher Education. She has worked with early childhood, elementary, middle, high school and adult language learners in rural, urban, suburban, and international school settings. She has extensive experience and training in Kagan cooperative learning, student centered instructional coaching, Harvard Project Zero and Visible Thinking Routines, SIOP, and Comprehensible Input Strategies. She has published materials for teaching adult English Learners with the TPRS method entitled Putting it Together, which have been translated into Spanish, Dutch, French, and sign language. She is a founding member and chair of the Eduliber board and member of the MyEdExpert Board.  Beth currently provides professional development, coaching, and consulting with schools around the world focused on providing equitable education for multilingual learners."
-  },
-  "Mia-Allen": {
-    "name": "Mia Allen - Founder",
-    "img": "MiaAllenHeadshot.png",
-    "bio": "Mia Ariela Allen is the Founder and Director of Professional Learning for Denver-based 4Ed Consulting. The organization is focused on improving programmatic structures and creating equitable learning opportunities for English Language Learners. Mia is currently completing dissertation work focused on English Language Learning programs for newcomer and refugee students that have timed-out or dropped-out of a traditional secondary school model. \n\n    Mia earned her first Masters at the University of Norther Colorado-Greely focused on Elementary Education and Culturally and Linguistically Diverse Students. Mia then went on to study at the University of Colorado-Boulder in the Bueno Center program- focused on bilingual special education. Mia has taught and coached in Denver Public schools and then went on to work in the digital publishing industry creating and support English Language Development and Spanish bilingual and biliteracy-based support resources. During this time in the publishing industry, Mia had the opportunity to provide WIDA-based workshops for many consortium member states. \n\n    Mia served as the Director of ELL and Bilingual Services for non-profit organization in Philadelphia where she assisted with an i3 scale grant in Broward County Schools (Florida), Denver Public Schoo(Colorado), Elizabeth Public Schools (New Jersey), Houston Independent School District. In her tenure, Mia supported the development of English language development resources and materials to create sheltered-learning and coaching aligned to early literacy development across the i3 project. Mia currently is working with school districts in Colorado, Illinois and New Mexico to develop language-rich learning enviornments that support cross-curricular English Language Development",
-  },
-  "Erin-Lyman": {
-    "name": "Erin Lyman - Treasurer",
-    "img": "ErinLymanHeadshot.png",
-    "bio": "Erin Lyman lives in Denver, Colorado and co-owner of the tax and accounting firm Newton Street CPA. Erin spends her time with her husband David Marsh and their two dogs Moses and Abel."
-  },
-  "Chanda-Austin": {
-    "name": "Chanda Austin - Member",
-    "img": "ChandaAustinHeadshot.png",
-    "bio": " Chanda Austin is a native of Tuscaloosa, AL and currently resides in Atlanta, GA. She holds a B.S. and a M.Ed. from Alabama A&amp;M University, Normal, AL and an Ed. S in School Leadership from Cambridge College, Cambridge, MA. She has been an educator for over twenty years serving in several different capacities and is passionate about people and learning. As a board member of EduLiber, her wealth of experiences in providing equable, sustainable learning opportunities to culturally and linguistically diverse students and educators will support strategic planning. She is a proud mother of an amazing, talented teenage girl."
-  },
-  "Israa-Hussein": {
-    "name": "Israa-Hussein - Member",
-    "img": "IsraaHusseinHeadshot.png",
-    "bio": "I am not a native to the education system but I have been an active member since 2009. I have a Bachelor's degree Psychology and I am currently pursuing a Masters in Healthcare policy. I believe in EduLIBER’s mission because I have first hand experience in how detrimental and ineffective some educators can be when it comes to second language learners and minorities."
-  },
-  "Joshua-Allen": {
-    "name": "Joshua Allen - Friend of the Board",
-    "bio": "Josh Allen is the Director of Technical Architecture and Strategy for Denver Public Schools (DPS). His position facilitates the enterprise and solutions architecture across both the academic and operations of Denver Public Schools. DPS is the fastest growing school district in the nation and as a result DPS has invested significantly in technology that allows Teachers, Principals, Students and Parents to manage their data in our district created portals. In his role, Josh ensures the success of the solutions that are implemented in schools, as well as central departments. Throughout his 20 years at DPS,  Josh has managed the district’s distance learning program, set up the district’s website, helped to build the first public school teacher pay for performance system and managed the district’s development, data warehouse and business intelligence teams.  Josh is a fourth generation Coloradoan and a former ECE and Montessori teacher",
-  },
-  "Jennifer-Archuleta": {
-    "name": "Jennifer Archuleta-Secretary",
-    "bio": "Jennifer has been in education for over 15 years and holds a Master’s Degree in Teaching and a principal license. She worked primarily with early childhood and in elementary settings before moving into the business side of education. She has extensive experience and training in educational assessment and grant writing. She is a founding member of the Eduliber board.  Beth currently provides grants programming and fiscal management for federal, state, and local grants for a Colorado school district."
-  },
-  "Marc-Liebman": {
-  "name": "Marc B. Liebman, Ph.D. -Friend of the Board",
-  "img": "MarcBLiebmanHeadshot.png",
-  "bio": "Marc is all about children and their success and helping them become successful learners, responsible citizens and tomorrow’s leaders. He has degrees in History, Human Development and Education and spent the first almost 40 years of his professional life as an educator in the public schools of California spending the first 8 years as a teacher and the last 18 as a district superintendent. He has also been a principal consultant for IBM and has just retired from being the CAO and Imagine Learning, an educational software company. Along the way he was named one of the 25 top education technology advocates in the United States and, with his wife of 49 years, founded and continue to operate the Mindy and Marc Liebman Scholarship Foundation. The foundation focuses on helping high risk, high potential students finish their schooling and getting their college degrees with a focus on giving back to their communities and helping others as part of their becoming our future leaders."
-  }
-}
 
 app.get("/board/:name", function(req, res) {
   var name = req.params.name;
---
+++ public/boardMemberData.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "Beth-Skelton": {
+    "name": "Beth Skelton - Chair",
+    "img": "BethSkeltonHeadshot.png",
+    "bio": " Beth has over 30 years of experience as a language educator and holds a Master’s Degree in Multicultural Teacher Education. She has worked with early childhood, elementary, middle, high school and adult language learners in rural, urban, suburban, and international school settings. She has extensive experience and training in Kagan cooperative learning, student centered instructional coaching, Harvard Project Zero and Visible Thinking Routines, SIOP, and Comprehensible Input Strategies. She has published materials for teaching adult English Learners with the TPRS method entitled Putting it Together, which have been translated into Spanish, Dutch, French, and sign language. She is a founding member and chair of the Eduliber board and member of the MyEdExpert Board.  Beth currently provides professional development, coaching, and consulting with schools around the world focused on providing equitable education for multilingual learners."
+  },
+  "Mia-Allen": {
+    "name": "Mia Allen - Founder",
+    "img": "MiaAllenHeadshot.png",
+    "bio": "Mia Ariela Allen is the Founder and Director of Professional Learning for Denver-based 4Ed Consulting. The organization is focused on improving programmatic structures and creating equitable learning opportunities for English Language Learners. Mia is currently completing dissertation work focused on English Language Learning programs for newcomer and refugee students that have timed-out or dropped-out of a traditional secondary school model. \n\n    Mia earned her first Masters at the University of Norther Colorado-Greely focused on Elementary Education and Culturally and Linguistically Diverse Students. Mia then went on to study at the University of Colorado-Boulder in the Bueno Center program- focused on bilingual special education. Mia has taught and coached in Denver Public schools and then went on to work in the digital publishing industry creating and support English Language Development and Spanish bilingual and biliteracy-based support resources. During this time in the publishing industry, Mia had the opportunity to provide WIDA-based workshops for many consortium member states. \n\n    Mia served as the Director of ELL and Bilingual Services for non-profit organization in Philadelphia where she assisted with an i3 scale grant in Broward County Schools (Florida), Denver Public Schoo(Colorado), Elizabeth Public Schools (New Jersey), Houston Independent School District. In her tenure, Mia supported the development of English language development resources and materials to create sheltered-learning and coaching aligned to early literacy development across the i3 project. Mia currently is working with school districts in Colorado, Illinois and New Mexico to develop language-rich learning enviornments that support cross-curricular English Language Development",
+  },
+  "Erin-Lyman": {
+    "name": "Erin Lyman - Treasurer",
+    "img": "ErinLymanHeadshot.png",
+    "bio": "Erin Lyman lives in Denver, Colorado and co-owner of the tax and accounting firm Newton Street CPA. Erin spends her time with her husband David Marsh and their two dogs Moses and Abel."
+  },
+  "Chanda-Austin": {
+    "name": "Chanda Austin - Member",
+    "img": "ChandaAustinHeadshot.png",
+    "bio": " Chanda Austin is a native of Tuscaloosa, AL and currently resides in Atlanta, GA. She holds a B.S. and a M.Ed. from Alabama A&amp;M University, Normal, AL and an Ed. S in School Leadership from Cambridge College, Cambridge, MA. She has been an educator for over twenty years serving in several different capacities and is passionate about people and learning. As a board member of EduLiber, her wealth of experiences in providing equable, sustainable learning opportunities to culturally and linguistically diverse students and educators will support strategic planning. She is a proud mother of an amazing, talented teenage girl."
+  },
+  "Israa-Hussein": {
+    "name": "Israa-Hussein - Secretary",
+    "img": "IsraaHusseinHeadshot.png",
+    "bio": "I am not a native to the education system but I have been an active member since 2009. I have a Bachelor's degree Psychology and I am currently pursuing a Masters in Healthcare policy. I believe in EduLIBER’s mission because I have first hand experience in how detrimental and ineffective some educators can be when it comes to second language learners and minorities."
+  },
+  "Joshua-Allen": {
+    "name": "Joshua Allen - Friend of the Board",
+    "bio": "Josh Allen is the Director of Technical Architecture and Strategy for Denver Public Schools (DPS). His position facilitates the enterprise and solutions architecture across both the academic and operations of Denver Public Schools. DPS is the fastest growing school district in the nation and as a result DPS has invested significantly in technology that allows Teachers, Principals, Students and Parents to manage their data in our district created portals. In his role, Josh ensures the success of the solutions that are implemented in schools, as well as central departments. Throughout his 20 years at DPS,  Josh has managed the district’s distance learning program, set up the district’s website, helped to build the first public school teacher pay for performance system and managed the district’s development, data warehouse and business intelligence teams.  Josh is a fourth generation Coloradoan and a former ECE and Montessori teacher",
+  },
+  "Marc-Liebman": {
+    "name": "Marc B. Liebman, Ph.D. -Friend of the Board",
+    "img": "MarcBLiebmanHeadshot.png",
+    "bio": "Marc is all about children and their success and helping them become successful learners, responsible citizens and tomorrow’s leaders. He has degrees in History, Human Development and Education and spent the first almost 40 years of his professional life as an educator in the public schools of California spending the first 8 years as a teacher and the last 18 as a district superintendent. He has also been a principal consultant for IBM and has just retired from being the CAO and Imagine Learning, an educational software company. Along the way he was named one of the 25 top education technology advocates in the United States and, with his wife of 49 years, founded and continue to operate the Mindy and Marc Liebman Scholarship Foundation. The foundation focuses on helping high risk, high potential students finish their schooling and getting their college degrees with a focus on giving back to their communities and helping others as part of their becoming our future leaders."
+  },
+  "Liliana-Suero": {
+    "name": "Liliana Suero- Member",
+    "img": "LilianaSueroHeadshot.jpg",
+    "bio": "Liliana Suero was born and raised in the Dominican Republic where she started her passion for learning and education. Once she arrived in the states at age 17, she learned her second and third language while graduating in college with a Bachelor of Arts in computer science. She then moved on to be an experienced and energetic bilingual educator for 21 years. She worked as an instructional coach, area coordinator and has delivered presentations at national conferences regarding bilingual education and Dual Language programs.  In 2006, she completed her master’s degree in Computer Education and Cognitive Systems at the University of North Texas.   Liliana has developed a plethora of presentations and workshops to enrich, motivate and guide teachers through the education profession. Since 2013 Liliana has been with Istation, a Texas-based Tech Ed company for the last 7 years. She began at Istation as the Spanish Manager of Curriculum Development and transitioned last year into the position of Instructional Design Director. Her main role is to oversee the development of digital instruction to address critical reading (English and Spanish) and math skills."
+  }
+}
