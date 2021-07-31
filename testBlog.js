f3eb0cfcf7934dd6402bd40e64c1345109c9479b
Edwin Savelson <esavelson279@gmail.com>
2021-07-30 20:01:01 -0600

    Create Test Blog Data

---
+++ testBlog.js
@@ -0,0 +1,39 @@
+const testBlogData = {
+  "Name": "Language Tree Online",
+  "Logo": "",
+  "Link": "",
+  "Email": "",
+  "Level": "",
+  "Format": "",
+
+  "Sections": [
+    {
+      "Title": "Something",
+      "Subheadings": [
+        {
+          "Title": "Something",
+          "Content": "Something"
+        },
+        {
+          "Title": "Something",
+          "Content": "Something"
+        }
+      ]
+    },
+    {
+      "Title": "Something",
+      "Subheadings": [
+        {
+          "Title": "Something",
+          "Content": "Something"
+        },
+        {
+          "Title": "Something",
+          "Content": "Something"
+        }
+      ]
+    }
+  ]
+
+}
+exports.testBlogData = testBlogData;
